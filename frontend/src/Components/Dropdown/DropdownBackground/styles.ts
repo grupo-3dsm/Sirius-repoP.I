10f3735797aa719b35ab866fae7eb30c4194b054
vbuarque <viniciusbuarque17@gmail.com>
2021-11-07 19:42:29 -0300

    Adicionando os arquivos do frontend

--- frontend/src/Components/Dropdown/DropdownBackground/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-import { motion } from 'framer-motion';
-
-export const Container = styled(motion.div)`
-  position: absolute;
-  bottom: 0;
-  background: #f6f9fc;
-  width: 100%;
-`;
--- frontend/src/Servidor/api.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'http://localhost:7000',
-})
-
-export default api;
